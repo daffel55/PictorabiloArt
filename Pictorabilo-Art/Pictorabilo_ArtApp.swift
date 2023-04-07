a787a0bd4bf62cf6ea19df97af9cd8c087d2e571
Dagmar Feldt <d.feldt@t-online.de>
2023-04-07 11:27:21 +0200

    Initial Commit

---
+++ Pictorabilo-Art/Pictorabilo_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pictorabilo_ArtApp.swift
+//  Pictorabilo-Art
+//
+//  Created by Dagmar Feldt on 07.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Pictorabilo_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
